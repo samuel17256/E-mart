d2674a54f7772311bd186df1dd9a68966fe76fe5
Promise278 <promiseobi2007@gmail.com>
2025-07-05 22:39:01 +0100

    feature: added the colors and fonts to the tailwindconfig and paymentmodal page

--- src/Components/Layout/PageLayout.jsx
+++ src/Components/Layout/PageLayout.jsx
@@ -1,13 +1,11 @@
 import Navbar from './Navbar'
 import { Outlet } from 'react-router'
 import Footer from './Footer'
-import PaymentModal from '../PaymentModal'
 
 const PageLayout = () => {
   return (
     <div className='min-h-screen grid grid-rows-[auto_1fr_auto]'>
         <Navbar />
-        <PaymentModal />
         <Outlet />
         <Footer />
     </div>
